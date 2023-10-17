20b13a5dda6d1f5f114fee54e40b895eafbddbd3
Robert Koeze <rkoeze@gmail.com>
2023-10-17 09:27:23 -0400

    Remove packer installation setup

--- install.sh
+++ install.sh
@@ -26,12 +26,3 @@ auth_github() {
 }
 
 export -f auth_github
-
-setup_vim() {
-  # https://github.com/wbthomason/packer.nvim#quickstart
-  git clone --depth 1 https://github.com/wbthomason/packer.nvim\
-    ~/.local/share/nvim/site/pack/packer/start/packer.nvim
-  # You'll then have to run :PackerInstall from nvim
-}
-
-export -f setup_vim
