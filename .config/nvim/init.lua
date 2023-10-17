23051c40cab7031c534c8b4ac81739178b9292a1
Robert Koeze <rkoeze@gmail.com>
2023-10-17 12:03:20 -0400

    Touch up vim init.lua

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -12,12 +12,6 @@ end
 vim.opt.rtp:prepend(lazypath)
 
 require('lazy').setup({
-  'wbthomason/packer.nvim',
-
-  'mattn/emmet-vim',
-
-  'tpope/vim-unimpaired',
-
   {
     'hrsh7th/nvim-cmp',
     dependencies = {'hrsh7th/cmp-buffer'}
@@ -35,6 +29,7 @@ require('lazy').setup({
   'nvim-treesitter/nvim-treesitter'
 })
 
+-- 'hrsh7th/nvim-cmp'
 local cmp = require'cmp'
 cmp.setup({
   mapping = cmp.mapping.preset.insert({
@@ -51,6 +46,7 @@ cmp.setup({
   })
 })
 
+-- 'nvim-telescope/telescope.nvim'
 require('telescope').setup{
   pickers = {
     find_files = {
@@ -59,7 +55,12 @@ require('telescope').setup{
   }
 }
 
+vim.api.nvim_set_keymap('n', '<leader>ff', '<cmd>Telescope find_files<cr>', {noremap = true, silent = true})
+vim.api.nvim_set_keymap('n', '<leader>fg', '<cmd>Telescope live_grep<cr>', {noremap = true, silent = true})
+vim.api.nvim_set_keymap('n', '<leader>fb', '<cmd>Telescope buffers<cr>', {noremap = true, silent = true})
+vim.api.nvim_set_keymap('n', '<leader>fh', '<cmd>Telescope help_tags<cr>', {noremap = true, silent = true})
 
+-- 'folke/tokyonight.nvim',
 require("tokyonight").setup({
   style = "night",
   on_highlights = function(hl, c)
@@ -69,7 +70,8 @@ require("tokyonight").setup({
   end,
 })
 
-
+vim.cmd("colorscheme tokyonight")
+-- 'dense-analysis/ale',
 vim.g.ale_linters = {
   python = {"flake8"}
 } 
@@ -92,15 +94,13 @@ vim.o.noswapfile=false
 -- map %% to expland filepath (used 1 line below)
 vim.cmd("cnoremap %% <C-R>=expand('%:h').'/'<cr>")
 vim.api.nvim_set_keymap('n', '<leader>e', ':edit %%', {noremap=true, silent=true})
-
 vim.api.nvim_set_keymap('n', '<leader>t', ':tabnew<cr>', {noremap=true, silent=true})
-
 vim.api.nvim_set_keymap('n', '<leader>o', ':Ex<cr>', {noremap=true, silent=true})
-
 vim.api.nvim_set_keymap('n', '<leader>n', ':nohlsearch<cr>', {noremap=true, silent=true})
 
 -- show line numbers
 vim.o.number=true
+
 -- when a bracket is inserted, briefly jump to the matching one.  T
 vim.o.showmatch=true
 vim.o.autoindent=true
@@ -115,9 +115,6 @@ vim.o.softtabstop=2
 vim.o.expandtab=true
 vim.o.shiftwidth=2 
 vim.o.smarttab=true
-
-
-vim.o.colorcolumn=80
 
 -- Always show tab bar at the top
 vim.o.showtabline=2
@@ -138,17 +135,12 @@ vim.o.autoread=true
 -- Use new regular expression engine
 vim.o.re=0
 
+-- File type specific commands
+-- Java
 vim.cmd[[autocmd Filetype java setlocal shiftwidth=4 tabstop=4]]
+-- Python
 vim.cmd[[autocmd FileType python setlocal tabstop=8 shiftwidth=4 softtabstop=4]]
-
--- Telescope
-vim.api.nvim_set_keymap('n', '<leader>ff', '<cmd>Telescope find_files<cr>', {noremap = true, silent = true})
-vim.api.nvim_set_keymap('n', '<leader>fg', '<cmd>Telescope live_grep<cr>', {noremap = true, silent = true})
-vim.api.nvim_set_keymap('n', '<leader>fb', '<cmd>Telescope buffers<cr>', {noremap = true, silent = true})
-vim.api.nvim_set_keymap('n', '<leader>fh', '<cmd>Telescope help_tags<cr>', {noremap = true, silent = true})
-
--- Tokyonight
-vim.cmd("colorscheme tokyonight")
+vim.cmd[[autocmd FileType python set colorcolumn=88]]
 
 vim.keymap.set("n", "=", [[<cmd>vertical resize +5<cr>]]) -- make the window biger vertically
 vim.keymap.set("n", "-", [[<cmd>vertical resize -5<cr>]]) -- make the window smaller vertically
